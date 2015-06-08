0886380738f3ca3452a3d820c9fd42d902adfa8a
Marlon Richert <marlon@vaadin.com>
2015-06-08 15:01:34 +0300

    add toolbar and card to demo

--- material-design-demo/src/main/java/org/vaadin/demo/DemoUI.java
+++ material-design-demo/src/main/java/org/vaadin/demo/DemoUI.java
@@ -1,23 +1,22 @@
 package org.vaadin.demo;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Alignment;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.NativeButton;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import org.vaadin.materialdesign.MaterialDesign;
+
+import javax.servlet.annotation.WebServlet;
 
 @Theme("material-design")
 @Title("Material Design for Vaadin 7 demo")
 @SuppressWarnings("serial")
-public class DemoUI extends UI
-{
-
+public class DemoUI extends UI {
     @WebServlet(value = "/*", asyncSupported = true)
     @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
     public static class Servlet extends VaadinServlet {
@@ -25,6 +24,46 @@ public class DemoUI extends UI
 
     @Override
     protected void init(VaadinRequest request) {
-        setContent(new NativeButton("Hello world!"));
+        setContent(new VerticalLayout() {
+            {
+                addComponent(new Toolbar());
+                addComponent(new Card() {
+                    {
+                        addComponent(new Actions() {
+                            {
+                                addComponent(new FlatButton("Disagree"));
+                                addComponent(new FlatButton("Agree"));
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    private class Toolbar extends HorizontalLayout {
+        public Toolbar() {
+            addStyleName(MaterialDesign.TOOLBAR);
+        }
+    }
+
+    private class Card extends VerticalLayout {
+        public Card() {
+            addStyleName(MaterialDesign.CARD);
+            setSizeUndefined();
+        }
+    }
+
+    private class Actions extends HorizontalLayout {
+        public Actions() {
+            addStyleName(MaterialDesign.ACTIONS);
+        }
+    }
+
+    private class FlatButton extends NativeButton {
+        public FlatButton(String caption) {
+            super(caption);
+            addStyleName(MaterialDesign.FLAT_BUTTON);
+        }
     }
 }
---
+++ material-design/src/main/java/org/vaadin/materialdesign/MaterialDesign.java
@@ -0,0 +1,9 @@
+package org.vaadin.materialdesign;
+
+public class MaterialDesign {
+    public static final String TOOLBAR = "md-toolbar";
+    public static final String CARD = "md-card";
+    public static final String DIALOG_BUTTON_CONTAINER = "md-dialog-button-container";
+    public static final String FLAT_BUTTON = "md-flat-button";
+    public static final String ACTIONS = "md-actions";
+}
